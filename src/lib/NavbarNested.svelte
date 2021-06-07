de8c721934ed2bd7d162beb5945406b636baeff4
DiAugust <diaugustdev@gmail.com>
2021-06-07 15:48:49 -0300

    solutions page added

--- src/lib/NavbarNested.svelte
+++ src/lib/NavbarNested.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Footer from './../../lib/Footer.svelte';
+
     let clicked = false;
     function menu() {
         if(clicked == false) {
@@ -23,12 +23,13 @@
         menu.style.display = 'none';
     }
 
-    import translations from '../../data/translations';
-    import { dict, locale, t } from '../../data/i18n';
+    import translations from '../data/translations';
+    import { dict, locale, t } from '../data/i18n';
     $: languages = Object.keys(translations);
     $: dict.set(translations);
 
 </script>
+
 <nav class="w-full h-40 flex justify-around items-center">
     <img src="../logo-full-white.svg" alt="" class="">
     <ul class="hidden sm:flex justify-around items-center">
@@ -38,7 +39,7 @@
         </li>
         {/each}
         <li class="mr-2 md:mr-4 lg:mr-11" id="divisor">|</li>
-        <a href="#" class=""><li class="mr-2 md:mr-4 lg:mr-11 px-8 py-3 rounded-full" id="contact-btn">{$t('contact')}</li></a>
+        <a href="#" class=""><li class="btn mr-2 md:mr-4 lg:mr-11 px-8 py-3 rounded-full" id="contact-btn">{$t('contact')}</li></a>
         <select bind:value={$locale} class="uppercase text-white" id="btn-lang">
             {#each languages as lang}
               <option value={lang} class="uppercase flex items-center justify-center">
@@ -62,47 +63,8 @@
     </li>
     {/each}
 </ul>
-<h1 id="navigation" class="text-white px-8 py-4 font-thin">Início > Soluções > Logística Integrada</h1>
-<div id="custom" class="w-full flex flex-col items-center justify-center">
-    <img src="../custom-desc.png" alt="">
-        <div class="flex flex-col lg:flex-row">
-            <div>
-                <h1 class="text-white text-2xl font-semibold max-w-lg text-center my-12 px-4">Nossa solução em logística integrada possui o acompanhamento a manuseio da sua carga do inicio ao fim.</h1>
-                <ul class="text-white">
-                    <li class="flex flex-row items-center my-8"><img id="icon" class="mx-4" src="../check.svg" alt="">Coleta da Carga na fábrica </li>
-                    <li class="flex flex-row items-center my-8"><img id="icon" class="mx-4" src="../check.svg" alt="">Recebimento da carga em nosso WH</li>
-                    <li class="flex flex-row items-center my-8"><img id="icon" class="mx-4" src="../check.svg" alt="">Conferência e separação dos volumes</li>
-                    <li class="flex flex-row items-center my-8"><img id="icon" class="mx-4" src="../check.svg" alt="">Desembaraço aduaneiro (origem e destino)</li>
-                    <li class="flex flex-row items-center my-8"><img id="icon" class="mx-4" src="../check.svg" alt="">Entrega da carga no destino final </li>
-                </ul>
-            </div>
-            <div class="flex flex-col items-center justify-center">
-                <h2 class="text-white text-xl font-thin my-8 max-w-md text-center">Sua carga sendo coletada e entregue com segurança, agilidade e acompanhamento de ponta a ponta.</h2>
-                <h2 class="text-white py-4">Quer saber mais? <a class="btn" href="#">Consulte-nos.</a></h2>
-            </div>
-        </div>
-</div>
+
 <style>
-
-    img {
-        mix-blend-mode: lighten;
-    }
-
-    #icon {
-        width: 30px;
-    }
-
-    #custom {
-        background-color: #0D0D2B;
-    }
-
-    #navigation {
-        background-color: #0D0D2B;
-    }
-
-    .btn {
-        color: #44B6C9;
-    }
 
     #btn-lang {
         background-color: #0D0D2B;
@@ -143,5 +105,4 @@
         0%   {background-color:#0D0D2B; height: 0px;}
         100% {background-color:#0D0D2B; height: 250px;}
     }
-
 </style>
---
+++ src/routes/solutions/truck.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Navbar from './../../lib/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<style>
+
+</style>
