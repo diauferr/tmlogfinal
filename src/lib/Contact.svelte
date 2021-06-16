45a21236dc9962a46a8dbc62e9b2fd8cb76aee44
DiAugust <diaugustdev@gmail.com>
2021-06-16 15:33:40 -0300

    form key changed

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -20,7 +20,7 @@ const showSurprise = () => {
 <div id="container" class="w-full flex flex-col items-center">
     <div class="login-box w-full max-w-2xl p-12 m-8 md:rounded-xl">
         <h2 class="text-3xl font-semibold text-center max-w-xl py-14 text-white">{$t('contact_data')[0].heading}</h2>
-        <form id="fs-frm" target="formSubmitFrame" name="survey-form" accept-charset="utf-8" action="https://formspree.io/f/mjvjwqaq" method="post" on:submit={showSurprise}>
+        <form id="fs-frm" target="formSubmitFrame" name="survey-form" accept-charset="utf-8" action="https://formspree.io/f/xeqvkrav" method="post" on:submit={showSurprise}>
           <fieldset id="fs-frm-inputs">
             <label class="text-white" for="full-name" id="margin">{$t('contact_data')[0].email_list[0].list1}</label>
             <input type="text" name="Nome" id="full-name" placeholder={$t('contact_data')[0].placeholder_list[0].list1} required>
